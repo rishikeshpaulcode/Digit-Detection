ebddcb1206640efd8fc421c9c849921573ab4c53
rishikeshpaulcode <rishikeshpaul.code@gmail.com>
2026-09-13 22:09:14 +0530

    fetch model prediction

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
+import cv2
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import load_model
+from tensorflow.keras import models
 
 # page configuration
 st.set_page_config(
@@ -18,9 +19,13 @@ if "display_predictions" not in st.session_state:
     st.session_state["display_predictions"] = False
 
 
+# load model
+model = models.load_model("model/digit_detection_model.keras")
+
+
 # app title and subtitle
-st.title(":green[Digit Detection App]")
-st.write(":grey[This is a Neural Network based basic Digit Detection and Classification app built using TensorFlow, Keras, NumPy, Streamlit.]")
+st.title(":blue[Digit Detection App]")
+st.write(":grey[This is a Neural Network based basic Digit Detection and Classification app built using TensorFlow, Keras, OpenCV, NumPy, and Streamlit.]")
 
 
 # main content layout (columns)
@@ -30,15 +35,10 @@ with col1:
     # input through drawable canvas
     st.subheader("Canvas Input:")
 
-    if st.get_option("theme.base") == "dark":
-        canvas_bg = "#02112e"
-    else:
-        canvas_bg = "#989799"
-
     canvas_result = st_canvas(
         stroke_width=28,
-        stroke_color="#27d604",
-        background_color=canvas_bg,
+        stroke_color="#055dff",
+        background_color="#000000",
         height=300,
         width=300,
         drawing_mode="freedraw",
@@ -53,7 +53,19 @@ with col2:
     st.subheader("Model Prediction:")
 
     if st.session_state["display_predictions"]:
-        st.subheader("Demo Output")
+        raw_image = canvas_result.image_data
+
+        if np.any(raw_image[:, :, :3]):
+            gray_image = cv2.cvtColor(raw_image.astype(np.uint8), cv2.COLOR_RGBA2GRAY)
+            resized_image = cv2.resize(gray_image, (28, 28), interpolation=cv2.INTER_AREA)
+            normalized_image = resized_image.astype("float64") / 255.0
+
+            model_predicted_array = model.predict(normalized_image[np.newaxis, :])
+            model_verdict = np.argmax(model_predicted_array)
+
+            st.write(model_verdict)
+        else:
+            st.warning("Empty canvas cannot be processed.", icon="⚠️")
 
         st.session_state["display_predictions"] = False
     else:
